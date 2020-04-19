a0fafc603eb4572af58fce632217b923e76e59a8
tannisroot <10602045+tannisroot@users.noreply.github.com>
2020-04-19 04:37:45 +0300

    Fix issue with url links and chromium

--- programs/winebrowser/main.c
+++ programs/winebrowser/main.c
@@ -69,6 +69,11 @@ static char *strdup_unixcp( const WCHAR *str )
     return ret;
 }
 
+static void restore_system_environment(void)
+{
+        setenv("LD_LIBRARY_PATH", "", 1);
+}
+
 /* try to launch a unix app from a comma separated string of app names */
 static int launch_app( const WCHAR *candidates, const WCHAR *argv1 )
 {
@@ -77,6 +82,11 @@ static int launch_app( const WCHAR *candidates, const WCHAR *argv1 )
     char **argv_new;
 
     if (!(cmdline = strdup_unixcp( argv1 ))) return 1;
+
+    /* PROTON HACK: Restore ORIG_LD_LIBRARY_PATH to LD_LIBRARY_PATH.
+     * System programs may not work correctly with our libraries, in
+     * particular gio on Ubuntu 19.04 is broken by our libgio. */
+    restore_system_environment();
 
     while (*candidates)
     {
